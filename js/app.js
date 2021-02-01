9c57a96df888e599c1167459d90a2bc20a9ded12
Fsesnick <fernandasesnick@gmail.com>
2021-02-01 19:06:30 +0000

    using on()

--- js/app.js
+++ js/app.js
@@ -9,7 +9,7 @@ $('.spoiler').prepend($button);
 // ocultar o spoiler no carregamento da página
 $('.spoiler span').hide();
 // quando o botão é pressionado,
-$('.spoiler button').click(function(){
+$('.spoiler button').on('click mouseleave', function(){
   //mostra o texto do spoiler 
   $('.spoiler span').show();
   //esconder o botão
