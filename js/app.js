af4fd8f27972fa0bc70fc47462dcd6ede25783f7
Fsesnick <fernandasesnick@gmail.com>
2021-02-01 21:46:57 +0000

    spoiler project final

--- js/app.js
+++ js/app.js
@@ -1,29 +1,15 @@
-
-/*
-// quando o botão é pressionado,
-$('.spoiler button').on('click', function(){
-  //mostra o texto do spoiler 
-  $('.spoiler span').show();
+$('.spoiler').on('click', 'button', function(){
+ //mostra o texto do spoiler 
+  $(this).prev().show();
   //esconder o botão
-  $('.spoiler button').hide();
-});
-*/
-
-$('.spoiler').on('click', 'button', function(event){
-  console.log(event);
-  //mostra o texto do spoiler 
-//  $('.spoiler span').show();
-  //esconder o botão
-  $(event.target).hide();
- 
+  $(this).hide(); 
 });
 
 //criar o botão Reveal Spoiler
 const $button = $('<button>Reveal Spoiler</button>');
 
 //anexar à página web.
-//$('.spoiler').append($button);
-$('.spoiler').prepend($button);
+$('.spoiler').append($button);
 
 // ocultar o spoiler no carregamento da página
 $('.spoiler span').hide();
