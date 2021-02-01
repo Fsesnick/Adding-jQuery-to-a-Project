2ce478cb61f0b3c7070bcb2ae54fc810239b7bf2
Fsesnick <fernandasesnick@gmail.com>
2021-02-01 17:35:59 +0000

    Spoiler revealer: Breaking it down

--- js/app.js
+++ js/app.js
@@ -1,4 +1,12 @@
-
+// ocultar o spoiler no carregamento da página
+$('.spoiler span').hide();
+// quando o botão é pressionado,
+$('.spoiler button').click(function(){
+  //mostra o texto do spoiler 
+  $('.spoiler span').show();
+  //esconder o botão
+  $('.spoiler button').hide();
+});
 
 
 
